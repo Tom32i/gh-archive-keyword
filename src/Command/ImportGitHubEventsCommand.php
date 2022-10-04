61274c4d2dd9979a2e8e5346a05fcf1fa4869b2f
Thomas Jarrand <thomas.jarrand@gmail.com>
2022-10-04 17:58:12 +0200

    Fixing env dev

--- src/Command/ImportGitHubEventsCommand.php
+++ src/Command/ImportGitHubEventsCommand.php
@@ -7,26 +7,23 @@ namespace App\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
 
 /**
  * This command must import GitHub events.
  * You can add the parameters and code you want in this command to meet the need.
  */
+#[AsCommand(
+    name: 'app:import-github-events',
+    description: 'Import GH events',
+)]
 class ImportGitHubEventsCommand extends Command
 {
-    protected static $defaultName = 'app:import-github-events';
-
-    protected function configure(): void
-    {
-        $this
-            ->setDescription('Import GH events');
-    }
-
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         // Let's rock !
         // It's up to you now
 
-        return 1;
+        return static::SUCCESS;
     }
 }
